2b5f082372936d165d10c637d5d6a26cbc7d1f45
Amit Rizal <amitrizal@hotmail.com>
2025-01-12 21:38:35 -0500

    Added module-2.zip file

---
+++ module-2/Rizal_mod2_2.HTML/Rizal_mod2_2.HTML/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Rizal_mod2_2.HTML.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
